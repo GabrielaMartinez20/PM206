5579900dc103ad06e663896a5f979b3b74e11588
GabrielaMartinez20 <123047794@upq.edu.mx>
2026-05-19 18:34:42 -0600

    Practica 2: parte del cliente
    Investigacion

---
+++ Practica_2/coffe.js
@@ -0,0 +1,14 @@
+//consultar productos 
+console.log(productos)
+
+//Crear pedido productos
+function pedido () {
+    return 
+}
+
+//Listar pedidos
+let pedidos = [
+    {nombre: },
+    {nombre: }
+]
+console.log(pedidos)
