fb9ac2e060b715e205b2dcced086ae0448c5272e
GabrielaMartinez20 <123047794@upq.edu.mx>
2026-05-21 09:26:59 -0600

    Modificaciones en Practica 2
    Agregue mas código

--- Practica_2/coffe.js
+++ Practica_2/coffe.js
@@ -1,14 +1,31 @@
-//consultar productos 
-console.log(productos)
+const productos = [
+    {nombre: "Producto 1", descripcion:"##"},
+    {nombre: "Producto 2", descripcion:"##"},
+    {nombre: "Producto 3", descripcion:"##"}
+];
+const pedidos = [
+    {nombre: "Pedido 1", cantidad:"2"},
+];
+    
+console.log(`¿Qué acción deseas realizar?
+1. Consultar productos
+2. Realizar un pedido 
+3. Consultar pedidos`)
 
-//Crear pedido productos
-function pedido () {
-    return 
+let index;
+if (index == 1)
+{
+    console.table(`Productos disponibles: 
+        ${productos}`)
 }
-
-//Listar pedidos
-let pedidos = [
-    {nombre: },
-    {nombre: }
-]
-console.log(pedidos)
+if (index == 2) {
+    //Crear pedido de productos
+    function agregarProducto (producto) {
+        productos.push(producto);
+    }
+} 
+if (index == 3) 
+    {
+        console.log(`Pedidos Reaizados:
+            ${pedidos}`) 
+    }
