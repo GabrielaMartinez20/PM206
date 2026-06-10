e754535f7eab25d92fb9a15605fa5478176fbc4b
GabrielaMartinez20 <123047794@upq.edu.mx>
2026-06-09 23:33:47 -0600

    Practica No.6: Estructura y Componentes
    Realizamos un componente propio como actividad de repaso el cual solo contiene 3 elementos de texto, usando la etiqueta "View", también se realizaron la importaciones necesarias y el código necesario de la zona 2  a "App.js" pero fueron en el commit anterior documentadas.

---
+++ Practica_5/miapp2/components/perfil.js
@@ -0,0 +1,15 @@
+import { Text, View} from "react-native";
+
+//funcion
+export const Perfil= () => {
+    return(
+        <View>
+
+        <Text>GABRIELA MARTINEZ CRUZ</Text>
+        <Text>Ingenieria en Sistemas Computacionales</Text>
+        <Text>Programacion Movil</Text>
+        <Text>Cuatrimestre 9°</Text>
+
+        </View>
+    )
+}
