19e1823f5cf8bdbfed74d1068305ec0a51bf2bb7
GabrielaMartinez20 <123047794@upq.edu.mx>
2026-06-12 22:44:25 -0600

    Practica No.7 Props & State
    1. Eliminamos componentes de saludos solo en "App.js". 2. Realizamos importaciones para hacer uso de props. 3. Agregamos props a el componente propio. Se puede por props o por desestructuración, aquí se debe hacer uso del método condicional y  de fragmentos. Realizamos la funcion de useState para que el botón despliegue la informacion de la persona .

--- Practica_5/miapp2/App.js
+++ Practica_5/miapp2/App.js
@@ -1,8 +1,7 @@
 /*Zona1: Importaciones de componentes & archivos */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { Saludo } from './components/Saludo';
-import { Saludo2 } from './components/Saludo2';
+
 import { Perfil } from './components/perfil';
 
 /*Zona2: Main - Hogar de los componentes */
@@ -10,16 +9,17 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text>-------Componentes Nativos------------</Text>
-      <Image source={require('./assets/wave.png')}></Image>
-      <Text>HOLA MUNDOOOOOOOO RN!</Text>
+      <Text>---------------------------Perfiles-------------------------</Text>
+      <Perfil nombre="Gabriela Martinez" carrera="Sistemas" materia="Programacion Movil" cuatri="9"></Perfil>
+      <Text>---------------------------------------------------------</Text>
+      <Perfil 
+      nombre="Isaias Garcia" 
+      carrera="Sistemas" 
+      materia="Programacion Movil" 
+      cuatri="9">
+      </Perfil>
 
-      <Text>-----------------Componente Simple-----------------</Text>
-      <Saludo></Saludo>
-      <Text>-----------------Componente Compuesto-----------------</Text>
-      <Saludo2></Saludo2>
-      <Text>-----------------Datos Personales-----------------</Text>
-      <Perfil></Perfil>
+
       <StatusBar style="auto" />
     </View>
   );
--- Practica_5/miapp2/components/perfil.js
+++ Practica_5/miapp2/components/perfil.js
@@ -1,15 +1,43 @@
+//Perfil utilizando props
+/* import { useState } from "react";
 import { Text, View} from "react-native";
 
 //funcion
-export const Perfil= () => {
+export const Perfil= (props) => {
     return(
         <View>
 
-        <Text>GABRIELA MARTINEZ CRUZ</Text>
-        <Text>Ingenieria en Sistemas Computacionales</Text>
-        <Text>Programacion Movil</Text>
-        <Text>Cuatrimestre 9°</Text>
+        <Text>{props.nombre}</Text>
+        <Text>{props.carrera}</Text>
+        <Text>{props.materia}</Text>
+        <Text>{props.cuatri}</Text>
 
         </View>
     )
-}
+} */
+ 
+//Perfil usando desestructuracion
+import { Text, View, Button} from "react-native";
+import React,{useState} from 'react';
+export const Perfil= ({nombre,carrera,materia,cuatri}) => {
+    const [mostrar,setMostrar]= useState(false)
+    return(
+        <View>
+
+        <Text>{nombre}</Text>
+
+        { mostrar &&  
+        <> 
+        <Text>{carrera}</Text>
+        <Text>{materia}</Text>
+        <Text>{cuatri}</Text>
+        </>
+        }
+        <Button 
+        title= "Mostrar perfil"
+        onPress={()=> setMostrar(!mostrar)}
+        />
+
+        </View>
+    )
+}  
