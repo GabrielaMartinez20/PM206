bbcb5efe94e51ba0b6b9831d5291c1f7d3af8902
GabrielaMartinez20 <123047794@upq.edu.mx>
2026-06-27 23:22:16 -0600

    Practica No.12: TextInput & Alert
    Componente de TextInput & Alert
    Se agrega texto en campos y alertas para errores o mensajes de éxito

--- Practica_5/miapp2/screens/TextInputScreen.js
+++ Practica_5/miapp2/screens/TextInputScreen.js
@@ -1,25 +1,53 @@
-/*Zona1: Importaciones de componentes & archivos */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, Platform, Alert, StyleSheet, Keyboard } from 'react-native';
 
+export default function TextInputScreen() {
+  const [nombre, setNombre] = useState('');
+  const [password, setPassword] = useState('');
+  const [edad, setEdad] = useState('');
+  const [correo, setCorreo] = useState('');
 
-/*Zona2: Main - Hogar de los componentes */
-export default function SafeAreaScreen() {
+  //FUNCION 
+  const procesarRegistro = () => {
+    if (Platform.OS !== 'web') Keyboard.dismiss();
+
+  //ALERTAS
+    if (!nombre || !password || !edad || !correo) {
+      alertasManager("Validación", "Todos los campos son obligatorios.");
+      return;
+    }
+
+    alertasManager("Éxito", `Registro procesado para: ${nombre}`);
+  };
+
+  const alertasManager = (titulo, mensaje) => {
+    if (Platform.OS === 'web') {
+      alert(`${titulo}: ${mensaje}`);
+    } else {
+      Alert.alert(titulo, mensaje);
+    }
+  };
+
   return (
     <View style={styles.container}>
-        <Text>Aqui va la practica 3</Text>
-      <StatusBar style="auto" />
+      {/* 1. TIPO TEXTO ESTÁNDAR */}
+      <TextInput style={styles.input} placeholder="Nombre Completo" value={nombre} onChangeText={setNombre} />
+      
+      {/* 2. TIPO CONTRASEÑA */}
+      <TextInput style={styles.input} placeholder="Contraseña" value={password} onChangeText={setPassword} secureTextEntry={true} />
+      
+      {/* 3. TIPO NUMÉRICO */}
+      <TextInput style={styles.input} placeholder="Edad" value={edad} onChangeText={setEdad} keyboardType="numeric" maxLength={3} />
+      
+      {/* 4. TIPO EMAIL */}
+      <TextInput style={styles.input} placeholder="Correo Electrónico" value={correo} onChangeText={setCorreo} keyboardType="email-address" autoCapitalize="none" autoCorrect={false} />
+
+      <Button title="Registrar Usuario" onPress={procesarRegistro} />
     </View>
   );
 }
 
-/*Zona3: estilos & posicionamiento */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column'
-  },
+  container: { flex: 1, justifyContent: 'center', padding: 20, backgroundColor: '#f5f6fa' },
+  input: { borderWidth: 1, borderColor: '#dcdde1', padding: 12, borderRadius: 8, marginBottom: 12, backgroundColor: '#fff' }
 });
