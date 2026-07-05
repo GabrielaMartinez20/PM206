4313e9e98b82250ad7bdb91eee4c5b67774ad521
GabrielaMartinez20 <123047794@upq.edu.mx>
2026-07-04 19:14:08 -0600

    Practica No. 18: Modal & BottomSheet
    Uso de modal y BottomSheet, se muestran por encima de la screen en donde se encuentran.

--- Practica_5/miapp2/screens/ModalScreen.js
+++ Practica_5/miapp2/screens/ModalScreen.js
@@ -1,25 +1,103 @@
-/*Zona1: Importaciones de componentes & archivos */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+/* Zona 1: Importaciones de componentes y archivos */
+import { StatusBar } from "expo-status-bar";
+import React, { use, useState } from "react";
+import { StyleSheet, Text, View, Button, Modal, Pressable, } from "react-native";
 
+/* Zona 2: Main - Hogar de los componentes */
+export default function ModalScreen() {
+    const [BottomSheet, setBottomSheet] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
-/*Zona2: Main - Hogar de los componentes */
-export default function SafeAreaScreen() {
-  return (
-    <View style={styles.container}>
-        <Text>Aqui va la practica 7</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return(
+        <View style = {styles.container}>
+            <Text style = {styles.titulo}>Ejemplo de Modal y BottomSheet</Text>
+            <Button color='#d3d604e1' title = "Abrir BottomSheet" onPress = {() => setBottomSheet(true)}/>
+
+            <Modal 
+                animationType = "slide" 
+                transparent = {true} 
+                visible = {BottomSheet}
+            >
+                <View style = {styles.fondo}>
+                    <View style = {styles.bottomSheet}>
+                        <Text style = {styles.texto}>Hola, esto es un BottomSheet</Text>
+                        <Pressable style = {styles.boton} onPress = {() => setBottomSheet(false)}>
+                            <Text style = {styles.textoBoton}>Cerrar</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            
+            <Button color='#d3d604e1' title = "Abrir Modal" onPress = {() => setModalVisible(true)}/>
+            <Modal 
+                animationType = "slide" 
+                transparent = {true} 
+                visible = {modalVisible}
+            >
+                <View style = {styles.fmodal}>
+                    <View style = {styles.modal}>
+                        <Text style = {styles.texto}>Hola, esto es un Modal</Text>
+                        <Pressable style = {styles.boton} onPress = {() => setModalVisible(false)}>
+                            <Text style = {styles.textoBoton}>Cerrar</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <StatusBar style = "auto"/>
+        </View>
+    );
 }
 
-/*Zona3: estilos & posicionamiento */
+/* Zona 3: Estilos y posicionamiento */
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column'
-  },
+    container: {
+        flex: 1, 
+        justifyContent: "center", 
+        alignItems: "center", 
+        backgroundColor: "#ffff", 
+    }, 
+    titulo: {
+        fontSize: 24, 
+        fontWeight: "bold", 
+        marginBottom: 20, 
+    }, 
+    fondo: {
+        flex: 1, 
+        justifyContent: "flex-end", 
+        backgroundColor: "rgba(0,0,0,0.4)", 
+    }, 
+    bottomSheet: {
+        backgroundColor: "#b009f1", 
+        padding: 25, 
+        borderTopLeftRadius: 20, 
+        borderTopRightRadius: 20, 
+        alignItems: "center", 
+    }, 
+    texto: {
+        fontSize: 16, 
+        marginBottom: 20, 
+    }, 
+    boton: {
+        backgroundColor: "#dee203e1", 
+        paddingHorizontal: 25, 
+        paddingVertical: 10, 
+        borderRadius: 40, 
+    }, 
+    textoBoton: {
+        color: "#fff", 
+        fontWeight: "bold", 
+    }, 
+    fmodal:{
+      flex:1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    modal: {
+      alignItems: "center",
+      justifyContent: "center",
+      height:200,
+      width:200,
+      backgroundColor:'#b009f1',
+      borderRadius: 40,
+    },
 });
